f2c3bba28bba3be090f23d8d517cd254513af9a8
karthikeyini07 <138025467+karthikeyini07@users.noreply.github.com>
2024-09-27 16:31:54 +0530

    27/09/2024

---
+++ stringrepeat.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class StringRepeat
+{
+public static void main(String[] args)
+{
+Scanner obj=new Scanner(System.in);
+int n=obj.nextInt();
+String s=sc.next();
+String res=repeatString(s,n);
+System.out.print(res);
+}
+}
