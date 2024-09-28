d5ac202c98e11f3daa55b16b17ce7534fb811a52
karthikeyini07 <138025467+karthikeyini07@users.noreply.github.com>
2024-09-28 09:54:12 +0530

    28/09/2024

--- stringrepeat.java
+++ stringrepeat.java
@@ -5,8 +5,18 @@ public static void main(String[] args)
 {
 Scanner obj=new Scanner(System.in);
 int n=obj.nextInt();
-String s=sc.next();
+String s=obj.next();
 String res=repeatString(s,n);
 System.out.print(res);
 }
+  public static  String repeatString(String s,int n)
+  {
+    StringBuilder sb=new StringBuilder();
+    for(int i=0;i<n;i++)
+      {
+        sb.append(s);
+      }
+    return sb.toString();
+  }
+    
 }
