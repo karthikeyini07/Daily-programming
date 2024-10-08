c0a26043d46be394814250035d104495e75a3265
karthikeyini07 <138025467+karthikeyini07@users.noreply.github.com>
2024-10-08 11:55:22 +0530

    Create FormatHour.java

---
+++ FormatHour.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Main 
+{
+    public static void main(String[] args)
+    {
+        Scanner obj=new Scanner(System.in);
+        int x=obj.nextInt();
+        int y=obj.nextInt();
+        int clk=x*y;
+        if(clk==0 || clk>24)
+        {
+            System.out.print(-1);
+        }
+        else if(clk<=12)
+        {
+            System.out.print(clk);
+        } 
+        else if(clk>12){
+            clk=clk-12;
+            System.out.print(clk);
+        }
+        
+    }
+    
+}
