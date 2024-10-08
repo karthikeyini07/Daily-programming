92256f39979e91bff6e716d6f561b44cb1276b4f
karthikeyini07 <138025467+karthikeyini07@users.noreply.github.com>
2024-10-08 10:37:39 +0530

    08/10/2024

---
+++ AreaOFTreeShadow.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner obj=new Scanner(System.in);
+	    int n=obj.nextInt();
+	    double area=3.14*n*n;
+	    System.out.print((int)(Math.ceil(area)));
+	    
+	}
+}
