571ef3fe43a3472e81aa260e8cf367d2c3cd15b2
karthikeyini07 <138025467+karthikeyini07@users.noreply.github.com>
2025-01-26 08:41:32 +0530

    26/01/2025
    to accept the string from the user and display the string in the password format without making any change in the input array

---
+++ Password.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class k
+{
+  public static void main(String[] args)
+  {
+    Scanner obj=new Scanner(System.in);
+    String str=obj.nextLine();
+    for(int i=0;i<str.length();i++)
+    {
+      System.out.print("*");
+    }
+  }
+}
